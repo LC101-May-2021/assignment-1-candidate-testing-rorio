a0357f1ca4934f03238c21e00163cba968b5886f
rkorio <>
2021-06-06 16:07:32 +0000

    Part 2.1 defined arrays questions correctAnswers

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,20 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
+let questions = [
+  "Who was the first American woman in space?",
+  "True or false: 5 kilometer == 5000 meters?",
+  "(5 + 3)/2 * 10 = ?",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+  "What is the minimum crew size for the ISS?"
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  40,
+  "Trajectory",
+  3
+];
 let candidateAnswers;
 
 
@@ -44,7 +56,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Nice to meet you " + candidateName);
+  console.log("Nice to meet you " + candidateNameSa);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
