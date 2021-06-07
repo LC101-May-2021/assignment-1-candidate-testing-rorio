bbafdebbf990d6d7d1677212b4227f527629848d
rkorio <>
2021-06-07 03:07:25 +0000

    Can't access index information from within the functions.

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,11 +9,11 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  "Who was the first American woman in space?",
-  "True or false: 5 kilometer == 5000 meters?",
-  "(5 + 3)/2 * 10 = ?",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
-  "What is the minimum crew size for the ISS?"
+  "1) Who was the first American woman in space? ",
+  "2) True or false: 5 kilometer == 5000 meters? ",
+  "3) (5 + 3)/2 * 10 = ? ",
+  "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "5) What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   "Sally Ride",
@@ -22,41 +22,47 @@ let correctAnswers = [
   "Trajectory",
   3
 ];
-let candidateAnswers;
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  let candidateName = input.question("What is your name? ");
-
+  let candidateName = input.question("Candidate Name: ");
+  
+  return candidateName;
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-let candidateAnswer = input.question(question);
+  let candidateAnswers = [];
 
+  for (let i = 0; i < questions.length; i++) {
+    let candidateAnswers = input.question(questions[i]);
+    console.log(`Your Answer: ${candidateAnswers}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+  
+  }
+  return candidateAnswers;
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (correctAnswer === candidateAnswer) {
-    console.log("Correct");
-  } else {
-    console.log("Incorrect");
-  }
-    
-
-  let grade;
+  // Calculating Grade Percentage (Number of Correct Answers) / (Number of Quiz Questions) * 100
+  let goodAnswer = [];
+  let grade = ((goodAnswer / questions.length) * 100);
   
-
+  if (String(candidateAnswers) === correctAnswers) {
+    goodAnswer[i++]; 
+  }
+  console.log(grade);
   return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Nice to meet you " + candidateNameSa);
+  //console.log("Nice to meet you " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
